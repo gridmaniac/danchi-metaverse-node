fbb760b6ed828c5447fc8b571e3ec5748fc1da9c
Anton Pushkarev <gridmaniac@gmail.com>
2022-02-18 23:02:32 +0900

    implement unsupported ownerOf method

--- modules/verificator.js
+++ modules/verificator.js
@@ -5,11 +5,11 @@ const authorizedList = {};
 
 const Web3 = require("web3");
 
-async function whoOwnsThisToken(address, tokenId) {
+function ownerOf(tokenId, address) {
   const web3 = new Web3(Web3.givenProvider || process.env.ETHEREUM_NODE);
-  const contract = new web3.eth.Contract(abi, address);
-  const owner = await contract.methods.ownerOf(tokenId).call();
-  return owner;
+  const contract = new web3.eth.Contract(abi, process.env.CONTRACT);
+  const balance = await contract.methods.balanceOf(address, tokenId).call();
+  return balance != 0;
 }
 
 module.exports.verifySignature = async function (message, signature, address) {
@@ -22,8 +22,8 @@ module.exports.verifyOwner = async function (nonce, tokenName, address) {
   if (tokenId == null)
     throw new Error("This aparment is not listed.");
 
-  const owner = await whoOwnsThisToken(process.env.CONTRACT, tokenId);
-  if (owner !== address) throw new Error("Ownership verification failed.");
+  const owner = await ownerOf(tokenId, address);
+  if (owner) throw new Error("Ownership verification failed.");
 
   // Authorize passed nonce
   authorizedList[nonce] = true;
